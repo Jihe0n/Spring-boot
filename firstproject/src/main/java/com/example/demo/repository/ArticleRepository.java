51bf32fbe399b470d7f1fa76d88e6e48cde4079d
Jiheon Kim <63222205+jiheoon@users.noreply.github.com>
2022-03-16 01:38:37 +0900

    Add files via upload

--- firstproject/src/main/java/com/example/demo/repository/ArticleRepository.java
+++ firstproject/src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.entity.Article;
 
